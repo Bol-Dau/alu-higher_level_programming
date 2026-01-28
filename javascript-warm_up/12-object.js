f89f028c514f9ef376fa3c222ff3800e9cfa2260
Bol <b.dau@alustudent.com>
2026-01-28 01:32:24 -0800

    Omo

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,7 +1,19 @@
 #!/usr/bin/node
-const myObject = {
+const myObj = {
+  type: 'object',
   value: 12
 };
-myObject.value = 89;
 
-console.log(`value: ${myObject.value}`);
+// Print the initial state
+console.log(myObj);
+
+// Update the value using the first command line argument
+if (process.argv[2] !== undefined) {
+  myObj.value = parseInt(process.argv[2]);
+} else {
+  // If no argument is passed, we manually set it to 89 to match your requirement
+  myObj.value = 89;
+}
+
+// Print the modified state
+console.log(myObj);
